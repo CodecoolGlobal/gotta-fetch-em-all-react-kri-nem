d6daa905666edea681cd4a7092d80a23cb3b83b4
Kristóf Németh <nemethkristof1@gmail.com>
2023-04-11 14:23:16 +0200

    implement basic structure of app

--- pokemon-app/src/App.jsx
+++ pokemon-app/src/App.jsx
@@ -1,9 +1,30 @@
 import './App.css'
+import React, { useState } from 'react'
+import World from './components/World'
+import Location from './components/Location'
+import Encounter from './components/Encounter'
 
-function App() {
+function handleLocationSelect () {
+}
+
+function handleOwnPokemonSelect () {
+}
+
+function handleEncounterEnd () {
+}
+
+function App () {
+  const [displayedComponent, setDisplayedComponent] = useState('World')
   return (
     <div className="App">
-
+      {displayedComponent === 'Locations'
+        ? <World onLocationSelect={handleLocationSelect}/>
+        : displayedComponent === 'Location'
+          ? <Location onOwnPokemonSelect={handleOwnPokemonSelect}/>
+          : displayedComponent === 'Encounter'
+            ? <Encounter onEncounterEnd={handleEncounterEnd}/>
+            : null
+      }
     </div>
   )
 }
---
+++ pokemon-app/src/components/Encounter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Encounter () {
+  return (
+    <></>
+  )
+}
+
+export default Encounter
