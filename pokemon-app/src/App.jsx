99766b1d5ee46ccb3d85671cd79e112e21734c86
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2023-04-13 10:03:26 +0200

    merge

--- pokemon-app/src/App.jsx
+++ pokemon-app/src/App.jsx
@@ -4,19 +4,11 @@ import World from "./components/World";
 import Location from "./components/Location";
 import Encounter from "./components/Encounter";
 
-<<<<<<< HEAD
-function App () {
-  const [displayedComponent, setDisplayedComponent] = useState('Location')
-  const [selectedLocationUrl, setSelectedLocationUrl] = useState('')
-  const [selectedPokemomonUrl, setSelectedPokemonUrl] = useState('')
-  const [encounteredPokemonUrl, setEncounteredPokemonUrl] = useState('')
-=======
 function App() {
   const [displayedComponent, setDisplayedComponent] = useState("World");
   const [selectedLocationUrl, setSelectedLocationUrl] = useState("");
   const [selectedPokemomonUrl, setSelectedPokemonUrl] = useState("");
   const [encounteredPokemonUrl, setEncounteredPokemonUrl] = useState("");
->>>>>>> development
 
   const [userPokemons, setUserPokemons] = useState([
     "https://pokeapi.co/api/v2/pokemon/bulbasaur",
@@ -44,23 +36,11 @@ function App() {
         onLocationSelect={handleLocationSelect}
         setDisplayedComponent={setDisplayedComponent}
       />
-<<<<<<< HEAD
-  } else if (displayedComponent === 'Location') {
-    activeComponent =
-      <Location
-        onOwnPokemonSelect={handlePokemonSelect}
-        areaPokemonsUrl="https://pokeapi.co/api/v2/location-area/115/"
-        userPokemons={userPokemons}
-      />
-  } else if (displayedComponent === 'Encounter') {
-    activeComponent =
-=======
     );
   } else if (displayedComponent === "Location") {
     activeComponent = <Location onOwnPokemonSelect={handlePokemonSelect} />;
   } else if (displayedComponent === "Encounter") {
     activeComponent = (
->>>>>>> development
       <Encounter
         onEncounterEnd={handleEncounterEnd}
         selectedPokemonUrl="https://pokeapi.co/api/v2/pokemon/bulbasaur"
