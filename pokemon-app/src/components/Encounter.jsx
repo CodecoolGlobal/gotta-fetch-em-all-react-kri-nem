11a0f21f6f6ae8990b40f2a0f673864a3a1b89a6
Kristóf Németh <nemethkristof1@gmail.com>
2023-04-13 07:46:22 +0200

    implement pokemon fight & fix linting problem

--- pokemon-app/src/components/Encounter.jsx
+++ pokemon-app/src/components/Encounter.jsx
@@ -1,61 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import { useImmer } from 'use-immer'
+import React, { useEffect, useState } from "react";
+import { useImmer } from "use-immer";
+import FightingPokemon from "./FightingPokemon";
 
-function Encounter ({ onEncounterEnd, selectedPokemonUrl, encounteredPokemonUrl }) {
-  const [ownPokemon, setOwnPokemon] = useImmer({})
-  const [encounteredPokemon, setEncounteredPokemon] = useImmer({})
-  const [attacker, setAttacker] = useState(null)
+function Encounter({
+  onEncounterEnd,
+  selectedPokemonUrl,
+  encounteredPokemonUrl,
+}) {
+  const [selectedPokemon, setSelectedPokemon] = useImmer(null);
+  const [encounteredPokemon, setEncounteredPokemon] = useImmer(null);
+  const [round, setRound] = useState(0);
 
   useEffect(() => {
-    Promise.all([
-      fetch(selectedPokemonUrl),
-      fetch(encounteredPokemonUrl)
-    ])
-      .then((res) => Promise.all(res.map(r => r.json())))
+    Promise.all([fetch(selectedPokemonUrl), fetch(encounteredPokemonUrl)])
+      .then((res) => Promise.all(res.map((r) => r.json())))
       .then((data) => {
-        setOwnPokemon(data[0])
-        setEncounteredPokemon(data[1])
-        setAttacker(data[0])
-      })
-  }, [selectedPokemonUrl, encounteredPokemonUrl])
+        setSelectedPokemon(data[0]);
+        setEncounteredPokemon(data[1]);
+      });
+  }, [selectedPokemonUrl, encounteredPokemonUrl]);
 
   const calculateAttack = (
     attack,
     defense,
     random = ((min = 217, max = 255) => {
-      const difference = max - min
-      let rand = Math.random()
-      rand = Math.floor(rand * difference)
-      rand = rand + min
-      return rand
+      const difference = max - min;
+      let rand = Math.random();
+      rand = Math.floor(rand * difference);
+      rand = rand + min;
+      return rand;
     })()
-  ) => ((((2 / 5 + 2) * attack * 60 / defense) / 50) + 2) * random / 255
+  ) => ((((2 / 5 + 2) * attack * 60) / defense / 50 + 2) * random) / 255;
 
-  const fight = (ownPokemon, encounteredPokemon) => {
+  const fight = () => {
+    const selectedAttack = selectedPokemon.stats[1].base_stat;
+    const selectedDefense = selectedPokemon.stats[2].base_stat;
+    const encounteredAttack = encounteredPokemon.stats[1].base_stat;
+    const encounteredDefense = encounteredPokemon.stats[2].base_stat;
 
-  }
+    const isSelectedPokemonsTurn = !(round % 2);
+    const isEncounterRunning =
+      selectedPokemon.stats[0].base_stat > 0 &&
+      encounteredPokemon.stats[0].base_stat > 0;
+    const isSelectedWinning = encounteredPokemon.stats[0].base_stat <= 0;
+
+    if (isSelectedPokemonsTurn && isEncounterRunning) {
+      setEncounteredPokemon((draft) => {
+        const newHp = Math.round(
+          draft.stats[0].base_stat -
+            calculateAttack(selectedAttack, encounteredDefense)
+        );
+        if (newHp > 0) {
+          draft.stats[0].base_stat = newHp;
+        } else {
+          draft.stats[0].base_stat = 0;
+        }
+      });
+      setRound(round + 1);
+    } else if (isEncounterRunning) {
+      setSelectedPokemon((draft) => {
+        const newHp = Math.round(
+          draft.stats[0].base_stat -
+            calculateAttack(selectedAttack, encounteredDefense)
+        );
+        if (newHp > 0) {
+          draft.stats[0].base_stat = newHp;
+        } else {
+          draft.stats[0].base_stat = 0;
+        }
+      });
+      setRound(round + 1);
+    } else {
+      if (isSelectedWinning) {
+        onEncounterEnd(encounteredPokemonUrl);
+      } else {
+        onEncounterEnd();
+      }
+    }
+  };
 
   return (
-    <> {ownPokemon && encounteredPokemon && (
-      <>
-        <div className="ownPoke">
-          <div className="pokeName">{ownPokemon.name}</div>
-          {ownPokemon.stats.map((stat, index) => (
-            <div key={index}>{stat.stat.name}: {stat.base_stat}</div>
-          ))}
-          <img className="pokeImage" src={ownPokemon.sprites.front_default}></img>
-        </div>
-        <div className="encounteredPoke">
-          <div className="pokeName">{encounteredPokemon.name}</div>
-          {encounteredPokemon.stats.map((stat, index) => (
-            <div key={index}>{stat.stat.name}: {stat.base_stat}</div>
-          ))}
-          <img className="pokeImage" src={encounteredPokemon.sprites.front_default}></img>
-        </div>
-        <button>Your turn</button>
-      </>)}
+    <>
+      {selectedPokemon && encounteredPokemon && (
+        <>
+          <FightingPokemon className="ownPoke" pokemon={selectedPokemon} />
+          <FightingPokemon
+            className="encounteredPoke"
+            pokemon={encounteredPokemon}
+          />
+        </>
+      )}
+      <button onClick={fight}>Try to catch</button>
     </>
-  )
+  );
 }
 
-export default Encounter
+export default Encounter;
